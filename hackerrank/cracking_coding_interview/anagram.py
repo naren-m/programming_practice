df817435f5b865a537bc47cde228d23a3986ceb6
Naren Mudivarthy <narenuday595@gmail.com>
2018-01-07 21:19:08 -0800

    hackerrank anagram working

--- hackerrank/cracking_coding_interview/anagram.py
+++ hackerrank/cracking_coding_interview/anagram.py
@@ -1,29 +1,48 @@
+
 def number_needed(a, b):
+    l = [0] * 26
+
+    for v in a:
+        l[97 - ord(v)] += 1
+
+
+    for v in b:
+        l[97 - ord(v)] -= 1
+
+    print(l)
+    s = 0
+    for v in l:
+        s += abs(v)
+    return s
+
+def number_needed1(a, b):
+    num_to_delete = 0
+    if a not in b:
+        print("Nothing common", len(set(a)) + len(set(b)))
+        # return len(set(a)) + len(set(b))
+
     d = dict()
     for l in a:
-        if l in d:
+        if d.has_key(l):
             d[l] = d[l] + 1
         else:
             d[l] = 1
+    print("Word", a, "dict", d)
+    d = dict()
     for l in b:
-        if l in d:
+        if d.has_key(l):
             d[l] = d[l] + 1
         else:
             d[l] = 1
 
-    num_to_delete = 0
+    print("Word", b, "dict", d)
     for k, v in d.items():
-        print(k, v)
-        if v <=1:
-            num_to_delete += 1
+        num_to_delete += abs(v)
 
-
-    print(d)
     return num_to_delete
 
-a = 'cde'
-b = 'abc'
+a = 'fcrxzwscanmligyxyvym'
+b = 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'
 
 print(number_needed(a, b))
-
 
