715c35d0f9dc4137ef0acdc49f32b4fc7b168000
Naren Mudivarthy <nmudivar@cisco.com>
2019-07-10 22:36:30 -0700

    Leetcode 437 705

---
+++ leetcode/705.design-hashset.py
@@ -0,0 +1,26 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        pass
+
+    def add(self, key: int) -> None:
+        pass
+
+    def remove(self, key: int) -> None:
+        pass
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        pass
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
