95f4532b9d3d17fcb8f38cba878b0102f00dd2d5
Naren Mudivarthy <nmudivar@cisco.com>
2020-03-28 22:24:50 -0700

    Euler 3

--- euler/problem_3.py
+++ euler/problem_3.py
@@ -1,18 +1,32 @@
 # https://projecteuler.net/problem=3
 
+import math
 
-def is_prime(num):
-    for i in range(2, num):
-        if num % i == 0:
-            return False
-    return True
+def sqrt(n):
+    return int(math.sqrt(n))
 
+def prime_factors(n):
+    pf = set()
+    while n %2 == 0:
+        n = n // 2
+        pf.add(2)
 
-for i in range(30):
-    print(i, is_prime(i))
+    factor = 3
+    max_factor = sqrt(n)
+    
+    while n > 1 and factor <= max_factor:
+        if n % factor == 0:
+            pf.add(factor)
+            while n % factor == 0:
+                n = n // factor 
+            max_factor = sqrt(n)
+        factor += 1
 
-def prime_num(max_num=0):
-    for i in range(max_num):
-        if 
+    if n != 1:
+        pf.add(n)
+    
+    return pf
 
-
+pfs = prime_factors(600851475143)
+print(pfs)
+print(max(pfs))
